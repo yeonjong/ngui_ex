0ffe98493b2225972dd218c69fc9795cfb6b9bc5
yeonjong <yeonjongjeong@nadagames.com>
2016-05-12 14:09:44 +0900

    complete ngui video tutorials

---
+++ ngui_ex/Assets/Scripts/ColoredSprite.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ColoredSprite : MonoBehaviour {
+
+	public void MakeItRed() {
+        GetComponent<UIWidget>().color = Color.red;
+    }
+
+    public void MakeItGreen()
+    {
+        GetComponent<UIWidget>().color = Color.green;
+    }
+
+    public void MakeItBlue()
+    {
+        GetComponent<UIWidget>().color = Color.blue;
+    }
+
+}
---
+++ ngui_ex/Assets/Scripts/SampleCharacter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SampleCharacter : MonoBehaviour {
+    
+    void OnHover(bool isOver) {
+        if (isOver) Debug.Log("Hover over " + name);
+        else Debug.Log("Hover away from " + name);
+    }
+
+    void OnPress(bool isPressed)
+    {
+        if (isPressed) Debug.Log("Pressed " + name);
+        else Debug.Log("Unpressed " + name);
+    }
+
+    void OnClick()
+    {
+        Debug.Log("Click on " + name);
+    }
+
+}
